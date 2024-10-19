b13d798cc532f6ac513a6b6cfb827d20477881ea
Biprodas Roy <biprodas.cse@gmail.com>
2024-10-20 00:37:04 +0600

    oo

--- frontend/app/(english-assessment)/ielts/_components/listening-answer-sheet.tsx
+++ frontend/app/(english-assessment)/ielts/_components/listening-answer-sheet.tsx
@@ -1,294 +1,41 @@
 import React from "react";
 import { Input } from "~/components/ui/input";
+import { cn } from "~/lib/utils";
 
 const ListeningAnswerSheet = () => {
   return (
     <div className="">
       <h4>Listening Answer Sheet</h4>
-      <table className="w-full">
-        <tbody>
-          <tr>
-            <td className="w-[10%]">
-              <code className="bg-blue-50 px-2 py-1 rounded-sm">01</code>
-            </td>
-            <td className="">
+      <div className="sm:flex">
+        <ul className="flex-1 space-y-2">
+          {Array.from({ length: 20 }, (_, idx) => (
+            <li key={idx} className="flex space-x-10">
+              <code
+                className={cn("px-2 py-1 rounded-sm bg-blue-50", {
+                  "bg-slate-50": idx & 1,
+                })}
+              >
+                {(idx + 1).toString().padStart(2, "0")}
+              </code>
               <Input className="w-[200px] h-8 px-2 shadow-none" />
-            </td>
-            <td className="w-[10%]">
-              <code className="bg-blue-50 px-2 py-1 rounded-sm">21</code>
-            </td>
-            <td className="">
+            </li>
+          ))}
+        </ul>
+        <ul className="flex-1 space-y-2">
+          {Array.from({ length: 20 }, (_, idx) => (
+            <li key={idx} className="flex space-x-10">
+              <code
+                className={cn("px-2 py-1 rounded-sm bg-blue-50", {
+                  "bg-slate-50": idx & 1,
+                })}
+              >
+                {(idx + 21).toString().padStart(2, "0")}
+              </code>
               <Input className="w-[200px] h-8 px-2 shadow-none" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <code className="bg-slate-50 px-2 py-1 rounded-sm">02</code>
-            </td>
-            <td>
-              <Input className="w-[200px] h-8 px-2 shadow-none" />
-            </td>
-            <td>
-              <code className="bg-slate-50 px-2 py-1 rounded-sm">22</code>
-            </td>
-            <td>
-              <Input className="w-[200px] h-8 px-2 shadow-none" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <code className="bg-blue-50 px-2 py-1 rounded-sm">03</code>
-            </td>
-            <td>
-              <Input className="w-[200px] h-8 px-2 shadow-none" />
-            </td>
-            <td>
-              <code className="bg-blue-50 px-2 py-1 rounded-sm">23</code>
-            </td>
-            <td>
-              <Input className="w-[200px] h-8 px-2 shadow-none" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <code className="bg-slate-50 px-2 py-1 rounded-sm">04</code>
-            </td>
-            <td>
-              <Input className="w-[200px] h-8 px-2 shadow-none" />
-            </td>
-            <td>
-              <code className="bg-slate-50 px-2 py-1 rounded-sm">24</code>
-            </td>
-            <td>
-              <Input className="w-[200px] h-8 px-2 shadow-none" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <code className="bg-blue-50 px-2 py-1 rounded-sm">05</code>
-            </td>
-            <td>
-              <Input className="w-[200px] h-8 px-2 shadow-none" />
-            </td>
-            <td>
-              <code className="bg-blue-50 px-2 py-1 rounded-sm">25</code>
-            </td>
-            <td>
-              <Input className="w-[200px] h-8 px-2 shadow-none" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <code className="bg-slate-50 px-2 py-1 rounded-sm">06</code>
-            </td>
-            <td>
-              <Input className="w-[200px] h-8 px-2 shadow-none" />
-            </td>
-            <td>
-              <code className="bg-slate-50 px-2 py-1 rounded-sm">26</code>
-            </td>
-            <td>
-              <Input className="w-[200px] h-8 px-2 shadow-none" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <code className="bg-blue-50 px-2 py-1 rounded-sm">07</code>
-            </td>
-            <td>
-              <Input className="w-[200px] h-8 px-2 shadow-none" />
-            </td>
-            <td>
-              <code className="bg-blue-50 px-2 py-1 rounded-sm">27</code>
-            </td>
-            <td>
-              <Input className="w-[200px] h-8 px-2 shadow-none" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <code className="bg-slate-50 px-2 py-1 rounded-sm">08</code>
-            </td>
-            <td>
-              <Input className="w-[200px] h-8 px-2 shadow-none" />
-            </td>
-            <td>
-              <code className="bg-slate-50 px-2 py-1 rounded-sm">28</code>
-            </td>
-            <td>
-              <Input className="w-[200px] h-8 px-2 shadow-none" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <code className="bg-blue-50 px-2 py-1 rounded-sm">09</code>
-            </td>
-            <td>
-              <Input className="w-[200px] h-8 px-2 shadow-none" />
-            </td>
-            <td>
-              <code className="bg-blue-50 px-2 py-1 rounded-sm">29</code>
-            </td>
-            <td>
-              <Input className="w-[200px] h-8 px-2 shadow-none" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <code className="bg-slate-50 px-2 py-1 rounded-sm">10</code>
-            </td>
-            <td>
-              <Input className="w-[200px] h-8 px-2 shadow-none" />
-            </td>
-            <td>
-              <code className="bg-slate-50 px-2 py-1 rounded-sm">30</code>
-            </td>
-            <td>
-              <Input className="w-[200px] h-8 px-2 shadow-none" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <code className="bg-blue-50 px-2 py-1 rounded-sm">11</code>
-            </td>
-            <td>
-              <Input className="w-[200px] h-8 px-2 shadow-none" />
-            </td>
-            <td>
-              <code className="bg-blue-50 px-2 py-1 rounded-sm">31</code>
-            </td>
-            <td>
-              <Input className="w-[200px] h-8 px-2 shadow-none" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <code className="bg-slate-50 px-2 py-1 rounded-sm">12</code>
-            </td>
-            <td>
-              <Input className="w-[200px] h-8 px-2 shadow-none" />
-            </td>
-            <td>
-              <code className="bg-slate-50 px-2 py-1 rounded-sm">32</code>
-            </td>
-            <td>
-              <Input className="w-[200px] h-8 px-2 shadow-none" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <code className="bg-blue-50 px-2 py-1 rounded-sm">13</code>
-            </td>
-            <td>
-              <Input className="w-[200px] h-8 px-2 shadow-none" />
-            </td>
-            <td>
-              <code className="bg-blue-50 px-2 py-1 rounded-sm">33</code>
-            </td>
-            <td>
-              <Input className="w-[200px] h-8 px-2 shadow-none" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <code className="bg-slate-50 px-2 py-1 rounded-sm">14</code>
-            </td>
-            <td>
-              <Input className="w-[200px] h-8 px-2 shadow-none" />
-            </td>
-            <td>
-              <code className="bg-slate-50 px-2 py-1 rounded-sm">34</code>
-            </td>
-            <td>
-              <Input className="w-[200px] h-8 px-2 shadow-none" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <code className="bg-blue-50 px-2 py-1 rounded-sm">15</code>
-            </td>
-            <td>
-              <Input className="w-[200px] h-8 px-2 shadow-none" />
-            </td>
-            <td>
-              <code className="bg-blue-50 px-2 py-1 rounded-sm">35</code>
-            </td>
-            <td>
-              <Input className="w-[200px] h-8 px-2 shadow-none" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <code className="bg-slate-50 px-2 py-1 rounded-sm">16</code>
-            </td>
-            <td>
-              <Input className="w-[200px] h-8 px-2 shadow-none" />
-            </td>
-            <td>
-              <code className="bg-slate-50 px-2 py-1 rounded-sm">36</code>
-            </td>
-            <td>
-              <Input className="w-[200px] h-8 px-2 shadow-none" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <code className="bg-blue-50 px-2 py-1 rounded-sm">17</code>
-            </td>
-            <td>
-              <Input className="w-[200px] h-8 px-2 shadow-none" />
-            </td>
-            <td>
-              <code className="bg-blue-50 px-2 py-1 rounded-sm">37</code>
-            </td>
-            <td>
-              <Input className="w-[200px] h-8 px-2 shadow-none" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <code className="bg-slate-50 px-2 py-1 rounded-sm">18</code>
-            </td>
-            <td>
-              <Input className="w-[200px] h-8 px-2 shadow-none" />
-            </td>
-            <td>
-              <code className="bg-slate-50 px-2 py-1 rounded-sm">38</code>
-            </td>
-            <td>
-              <Input className="w-[200px] h-8 px-2 shadow-none" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <code className="bg-blue-50 px-2 py-1 rounded-sm">19</code>
-            </td>
-            <td>
-              <Input className="w-[200px] h-8 px-2 shadow-none" />
-            </td>
-            <td>
-              <code className="bg-blue-50 px-2 py-1 rounded-sm">39</code>
-            </td>
-            <td>
-              <Input className="w-[200px] h-8 px-2 shadow-none" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <code className="bg-slate-50 px-2 py-1 rounded-sm">20</code>
-            </td>
-            <td>
-              <Input className="w-[200px] h-8 px-2 shadow-none" />
-            </td>
-            <td>
-              <code className="bg-slate-50 px-2 py-1 rounded-sm">40</code>
-            </td>
-            <td>
-              <Input className="w-[200px] h-8 px-2 shadow-none" />
-            </td>
-          </tr>
-        </tbody>
-      </table>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
